67e3b5af5e57ee5867f08d726c8713e0e1d7783a
Thien Nguyen <Thiennguyenwsb@gmail.com>
2018-11-22 18:46:28 -0800

    register

--- views/components/signup.vue
+++ views/components/signup.vue
@@ -3,38 +3,43 @@
           <v-flex xs12 sm8 md4>
             <v-card class="elevation-12">
               <v-toolbar dark color="primary">
-                <v-toolbar-title>Login form</v-toolbar-title>
+                <v-toolbar-title>Signup form</v-toolbar-title>
                 <v-spacer></v-spacer>
                
               </v-toolbar>
               <v-card-text>
                 <v-form>
-                  <v-text-field prepend-icon="person" v-model="email" label="Email" type="text"></v-text-field>
+                  <v-text-field prepend-icon="person" v-model="name" label="Name" type="text"></v-text-field>
+                  <v-text-field prepend-icon="person" v-model="email" label="Email" type="email"></v-text-field>
                   <v-text-field id="password" prepend-icon="lock" v-model="password" label="Password" type="password"></v-text-field>
                 </v-form>
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="primary" @click='logIn'>Login</v-btn>
+                <v-btn color="primary" type='submit' @click='registerUser'>Signup</v-btn>
               </v-card-actions>
             </v-card>
           </v-flex>
         </v-layout>
 </template>
 
-
 <script>  
 export default {
     data(){
       return {
         email:null,
+        name:null,
         password:null
       }
     },
     methods: {
-      logIn(){
-        this.$store.dispatch('/api/auth/login/',{email:this.email,password:this.password}).then(()=>{
-            this.$router.push('/');
+      registerUser(){
+        axios.post('/api/register',{name:this.name,email:this.email,password:this.password})
+        .then((response)=>{
+            Bus.$emit('loggedIn',response);
+        })
+        .catch((error)=>{
+            console.log(error);
         })
         
       }
