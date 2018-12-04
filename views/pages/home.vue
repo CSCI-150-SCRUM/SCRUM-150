0de62910548a631bdc67df6c069ad8a254651d4d
Thien Nguyen <Thiennguyenwsb@gmail.com>
2018-12-04 14:43:36 -0800

    front end home page

--- views/pages/home.vue
+++ views/pages/home.vue
@@ -4,17 +4,17 @@
       <template>
         <v-responsive>
           <header>
-            <h1 style="font-size:64px">WELCOME TO SCRUMBAG</h1>
+            <h1 style="font-size:44px" class="text-xs-center">WELCOME TO SCRUMBAG</h1>
           </header>
-          <p style="color:DodgerBlue;">A VISUAL MANAGEMENT TOOL!</p>
+          <p class="text-xs-center" style="color:DodgerBlue;">A VISUAL MANAGEMENT TOOL!</p>
           <hr class="my-4">
 
           <v-container grid-list-md>
             <v-layout wrap>
-              <v-flex v-for="item in items" :key="item" xs6 sm4 md3>
-                <v-card>
+              <v-flex v-for="item in items" :key="item" xs6 sm4 md3 lg2>
+                <v-card height="200px">
                     <v-icon dark>{{item.icons}}</v-icon>
-                    <v-card-title>{{item.titles}}</v-card-title>
+                    <v-card-text class="text-xs-center">{{item.titles}}</v-card-text>
                 </v-card> 
               </v-flex>
             </v-layout>
@@ -33,10 +33,15 @@ export default {
       items: [
         { icons: "home", titles: "Home" },
         { icons: "settings", titles: "Settings" },
-        { icons: "group", titles: "Contacts" },
+        { icons: "contacts", titles: "Contacts" },
         { icons: "create_new_folder", titles: "New Project" },
+        { icons: "group", titles: "Groups" },
         { icons: "work", titles: "Existing Project" },
         { icons: "event", titles: "Calendar" },
+        { icons: "chat", titles: "Chat" },
+        { icons: "done", titles: "Completed Project" },
+        { icons: "help", titles: "Help" }
+        
       ]
     }),
   
@@ -55,13 +60,13 @@ export default {
 
 <style scoped>
   .v-icon{
-    font-size: 180px;
+    font-size: 150px;
   }
 
   .v-card{
     text-align: center;
     margin: auto;
-    display:inline-block;
+    display:block;
     background-color: transparent!important;
     border-color: transparent!important;
   }
