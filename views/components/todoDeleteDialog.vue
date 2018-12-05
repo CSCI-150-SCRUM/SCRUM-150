a22a8cabf3269489bfb4250b336eda26c2b57e35
unknown <kristenl.1281@gmail.com>
2018-12-05 13:41:57 -0800

    working on todo and done deletes

--- views/components/todoDeleteDialog.vue
+++ views/components/todoDeleteDialog.vue
@@ -1,21 +1,16 @@
 <template>
   <v-card>
-    <v-toolbar class="primary primaryText--text">
-      <v-toolbar-title>Delete todo tasks Record {{deleteName}}</v-toolbar-title>
+     <v-toolbar class="primary primaryText--text" >
+      <v-toolbar-title> Delete task Record {{deleteName}} </v-toolbar-title>
     </v-toolbar>
 
     <v-card-text>
-      <p>
-        This action will remove {{deleteName}} from the application. This is
-        <strong>irreversible.</strong>
+      <p>This action will remove {{deleteName}} from the application. This is
+      <strong>irreversible.</strong>
       </p>
     </v-card-text>
     <v-card-actions>
-      <v-btn
-        @click="confirmDelete()"
-        :loading="!deleteDone"
-        class="red darken-2 white--text"
-      >Confirm</v-btn>
+      <v-btn @click="confirmDelete()" :loading="!deleteDone" class="red darken-2 white--text">Confirm</v-btn>
       <v-spacer></v-spacer>
       <v-btn @click="close()" class="green lighten-1 white--text">Cancel</v-btn>
     </v-card-actions>
@@ -23,50 +18,57 @@
 </template>
 
 <script>
-import { http } from "../config/http";
+import { http } from '../config/http';
 
 export default {
   data: () => ({
-    deleteDone: true
+    deleteDone: true,
   }),
 
   props: {
-    todo: {
-      type: Object
+    task: {
+      type: Object,
     },
     deleteName: {
       type: String,
-      default: ""
+      default: '',
     }
   },
 
   methods: {
     confirmDelete() {
+      if(this.task.status=="To Do"){
       this.deleteDone = false;
       http
-        .delete("/todo/" + this.Todo._id)
+        .delete('/tasks/' + this.task._id)
         .then(response => {
           this.deleteDone = true;
-          this.alert(true, "Delete", "Todo");
+          this.alert(true, 'Delete', 'To-do');
+
           this.close();
+          
         })
         .catch(e => {
           this.deleteDone = true;
-          this.alert(false, "Delete", "Todo");
+          this.alert(false, 'Delete', 'To-do');
           this.close();
         });
+      }
+      
     },
 
+   
+
     close() {
-      this.$emit("closeDelete");
+      this.$emit('closeDelete');
     },
 
     alert(success, callName, resource) {
-      this.$emit("alert", success, callName, resource);
-    }
+      this.$emit('alert', success, callName, resource);
+    },
   },
 
-  mounted() {}
+  mounted() {},
 };
 </script>
 
