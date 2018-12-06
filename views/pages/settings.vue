2a7da19a0e2d11ca7a735175bbd93261fef55532
Thien Nguyen <Thiennguyenwsb@gmail.com>
2018-12-05 21:23:28 -0800

    haha

--- views/pages/settings.vue
+++ views/pages/settings.vue
@@ -2,27 +2,29 @@
   <v-container grid-list-md>
     <v-responsive>
       <v-layout wrap row justify-center align-center>
-        <v-flex xs4 sm4 md3 lg2>
-          <v-btn color="purple">Profile</v-btn>
+        <v-flex xs6 sm4 md3 lg2>
+          <v-btn block color="purple">Profile</v-btn>
+          <br>
+        </v-flex>
+        <v-flex xs6 sm4 md3 lg2>
+          <v-btn block color="red">Change Password</v-btn>
+          <br>
+        </v-flex>
+        <v-flex xs6 sm4 md3 lg2>
+          <v-btn block color="orange">Light Mode</v-btn>
           <br>
         </v-flex>
         <v-flex xs4 sm4 md3 lg2>
-          <v-btn color="red">Change Password</v-btn>
+          <v-btn block color="green">Change Username</v-btn>
           <br>
         </v-flex>
-        <v-flex xs4 sm4 md3 lg2>
-          <v-btn color="orange">Light Mode</v-btn>
+        <v-flex xs6 sm4 md3 lg2>
+          <v-btn block color="pink">Edit Font</v-btn>
           <br>
         </v-flex>
-        <v-flex xs4 sm4 md3 lg2>
-          <v-btn color="green">Change Username</v-btn>
-          <br>
+          <v-flex xs6 sm4 md3 lg2>
+          <v-btn block color="light blue">Connect to Facebook</v-btn>
         </v-flex>
-        <v-flex xs4 sm4 md3 lg2>
-          <v-btn color="pink">Edit Font</v-btn>
-          <br>
-        </v-flex>
-        <v-btn color="light blue">Connect to Facebook</v-btn>
       </v-layout>
     </v-responsive>
   </v-container>
